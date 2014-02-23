4e16c5880edb460bdaeb88e3feb3b9b12d476cdc
Alastair Lynn <arplynn@gmail.com>
2014-02-23 00:13:19 +0000

    Add main.lua comment for plugin name

--- cronoplug/plugin.py
+++ cronoplug/plugin.py
@@ -1,15 +1,23 @@
 import zipfile
 
 class Plugin:
-    def __init__(self, stream):
+    def __init__(self, stream, name):
         self.file = zipfile.ZipFile(stream, 'w')
+        self._wrote_main = False
+        self.name = name
 
     def __enter__(self):
         return self
 
     def add_file(self, name, content):
-        self.file.writestr(name, content)
+        info = zipfile.ZipInfo(filename=name)
+        if name == 'main.lua':
+            self._wrote_main = True
+            info.comment = self.name
+        self.file.writestr(info, content)
 
     def __exit__(self, type, value, traceback):
+        if not self._wrote_main:
+            self.add_file(name = 'main.lua', content = b'')
         self.file.close()
 
--- cronoplug/test_plugin.py
+++ cronoplug/test_plugin.py
@@ -5,11 +5,19 @@ import zipfile
 
 def test_base():
     bio = io.BytesIO()
-    with Plugin(bio) as plg:
+    with Plugin(bio, "Test Plugin") as plg:
         plg.add_file(name = 'main.lua',
                      content = 'I am covered in bees.')
     zp = zipfile.ZipFile(bio, 'r')
     with zp.open('main.lua') as f:
         content = f.read()
         assert content == 'I am covered in bees.'
+
+def test_info():
+    bio = io.BytesIO()
+    with Plugin(bio, "Test Plugin") as plg:
+        pass
+    zp = zipfile.ZipFile(bio, 'r')
+    info = zp.getinfo('main.lua')
+    assert info.comment == "Test Plugin"
 
